4a0941348bd4851184bc7fe3c54225f624d4c2d0
Andrew Zhilenko <andrew@ti.cz>
2022-04-27 16:30:09 +0200

    Fix exception handling
    Not sure what was the deal with two separate constructors for MiroException class, but it was not working. Fixed so a clear message (with error response from Miro API) will be printed

--- miro/exceptions.py
+++ miro/exceptions.py
@@ -1,16 +1,22 @@
 class MiroException(Exception):
 
     def __init__(self, status: int, details: str = ''):
+        super(MiroException, self).__init__()
         self.status = status
         self.details = details
 
-    def __init__(self, cause: Exception):
-        super(MiroException, self).__init__(cause)
+    def __str__(self):
+        return f'Exception {type(self).__name__} err code = {self.status} extended details: {self.details}'
+
+    # def __init__(self, cause: Exception):
+    #     super(MiroException, self).__init__(cause)
 
 
 class InvalidCredentialsException(MiroException):
     """An authentication error occurred because of bad credentials."""
 
+class APIErrorException(MiroException):
+    """API error - e.g. wrong parameters"""
 
 class ObjectNotFoundException(MiroException):
     """The requested object was not found."""
